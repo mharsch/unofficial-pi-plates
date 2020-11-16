f95a45dff41a3abbd7808d89d2478e8f740a01b5
Mike Harsch <mike@harschsystems.com>
2020-11-16 09:18:53 -0800

    5.22

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='pi-plates',
-    version='5.21',
+    version='5.22',
     license='BSD',
     author='Jerry Wasinger, WallyWare, inc.',
     author_email='support@pi-plates.com',
